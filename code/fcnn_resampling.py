b4ee75b2d62e843257488ef181410b3a2d0ab008
Alison1iu <150476969@qq.com>
2022-05-09 23:03:02 -0400

    testing on diferet level

--- code/fcnn_resampling.py
+++ code/fcnn_resampling.py
@@ -11,25 +11,22 @@ from sklearn.model_selection import KFold
 file_name = 'combined_data/51features_RF.csv'
 
 def resampling_data(input_file):
+
     data = pd.read_csv(input_file)
 
     #print(data['OHAREC'].value_counts())
     class_count_4, class_count_3,class_count_2,class_count_1 = data['OHAREC'].value_counts()
     print(class_count_1, class_count_2,class_count_3,class_count_4)
+
     # Separate class
-    class_1 = data[data['OHAREC'] == 1]# print the shape of the class
+    class_1 = data[data['OHAREC'] == 1]
     class_2 = data[data['OHAREC'] == 2]
     class_3 = data[data['OHAREC'] == 3]
     class_4 = data[data['OHAREC'] == 4]
 
-    # print('class 1:', class_1.shape)
-    # print('class 2:', class_2.shape)
-    # print('class 3:', class_3.shape)
-    # print('class 4:', class_4.shape)
-
-    class_3_under = class_3.sample(class_count_2)
-    class_4_under = class_4.sample(class_count_2)
-    class_1_over = class_1.sample(100, replace=True)
+    class_3_under = class_3.sample(3000)
+    class_4_under = class_4.sample(3000)
+    class_1_over = class_1.sample(500, replace=True)
 
     test_under = pd.concat([class_1_over,class_2, class_3_under, class_4_under], axis=0)
 
@@ -62,8 +59,8 @@ def get_data(data):
 
     return train_X, train_Y, test_X, test_Y
 
-
 data = resampling_data(file_name)
+#data = pd.read_csv(file_name)
 train_X, train_Y, test_X, test_Y = get_data(data)
 model = Sequential([
     Dense(60, activation='relu'),
@@ -79,4 +76,38 @@ hist = model.fit(train_X, train_Y,
            batch_size=100, epochs=20,
            validation_data=(test_X, test_Y))
 
-print(model.evaluate(test_X, test_Y)[1])
+# The following is testing on differet level
+data = pd.read_csv(file_name)
+class_1 = data[data['OHAREC'] == 1]
+class_2 = data[data['OHAREC'] == 2]
+class_3 = data[data['OHAREC'] == 3]
+class_4 = data[data['OHAREC'] == 4]
+class_3_under = class_3.sample(500)
+class_4_under = class_4.sample(500)
+class_2_under = class_2.sample(500)
+
+test_under = pd.concat([class_1,class_2_under, class_3_under, class_4_under], axis=0)
+test_under = test_under.values
+
+# Standardization
+scaler = preprocessing.StandardScaler().fit(test_under)
+test_under = scaler.transform(test_under)
+# Scaling features to a range (0,1)
+min_max_scaler = preprocessing.MinMaxScaler()
+data = min_max_scaler.fit_transform(test_under)
+
+x = data[:12, :-1]
+y = data[:12, -1]
+print("The accurcy on level 1 is:",model.evaluate(x, y)[1])
+
+x = data[13:512, :-1]
+y = data[13:512, -1]
+print("The accurcy on level 2 is:",model.evaluate(x, y)[1])
+
+x = data[513:1012, :-1]
+y = data[513:1012, -1]
+print("The accurcy on level 3 is:",model.evaluate(x, y)[1])
+
+x = data[1013:1512, :-1]
+y = data[1013:1512, -1]
+print("The accurcy on level 4 is:",model.evaluate(x, y)[1])
